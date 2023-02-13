f74dba28046931ffa33f80b7c887cae29fbe12c5
Tringapps Subhiksha <123531014+subhi-29@users.noreply.github.com>
2023-02-13 12:12:50 +0530

    Update Calculatornew.java

--- src/main/java/org/example/Calculatornew.java
+++ src/main/java/org/example/Calculatornew.java
@@ -10,16 +10,14 @@ class Calculator{
         this.num1=first;
         this.num2=second;
     }
-    double calc(){
-        return 0;
-
-    }
+    
 }
 class Add extends Calculator{
     Add(double first,double second){
         super(first,second);
     }
     double calc(){
+        @Override
         return num1+num2;
     }
 }
@@ -28,6 +26,7 @@ class Sub extends Calculator{
         super(first,second);
     }
     double calc(){
+        @Override
         return num1-num2;
     }
 }
@@ -36,6 +35,7 @@ class Mul extends Calculator{
         super(first,second);
     }
     double calc(){
+        @Override
         return num1*num2;
     }
 }
@@ -44,6 +44,7 @@ class Div extends Calculator{
         super(first,second);
     }
     double calc(){
+        @Override
         return num1/num2;
     }
 }
@@ -56,7 +57,7 @@ public class Calculatornew {
         l.info("Enter the Second Number:");
         double second=sc.nextDouble();
         Calculator c=new Calculator(first,second);
-        c.calc();
+        
         Calculator c1=new Add(first,second);
         Calculator c2=new Sub(first,second);
         Calculator c3=new Mul(first,second);
@@ -69,13 +70,13 @@ public class Calculatornew {
         char operator = sc.next().charAt(0);
         switch(operator){
             case '+':
-                l.info("Addition of two numbers: "+c1.calc());
+               l.log(Level.INFO, () ->"Addition of two numbers: "+c1.calc());
                 break;
             case '-':
-                l.info("Subtraction of two numbers: "+c2.calc());
+                  l.log(Level.INFO, () ->"Subtraction of two numbers: "+c2.calc());
                 break;
             case '*':
-                l.info("Multiplication of two numbers: "+c3.calc());
+                 l.log(Level.INFO, () ->"Multiplication of two numbers: "+c3.calc());
                 break;
             case '/':
                 l.info("Division of two numbers: "+c4.calc());
